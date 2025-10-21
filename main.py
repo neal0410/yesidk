031cef1f4e5eb9c17e7c5f89d7a96f2579014701
neal0410 <neal041010@gmail.com>
2025-10-21 16:56:55 +0700

    Add comments and improve salary prediction display

--- main.py
+++ main.py
@@ -1,20 +1,25 @@
-
+# 1 import library
 from sklearn.linear_model import LinearRegression
 import pandas as pd
 import matplotlib.pyplot as plt
 import streamlit as st
 
+# 2 menyiapkan data
 X = [[1], [2], [3], [4]] #tipe data list 2 dimensi
 y = [101, 102, 103, 104] #tipe data list 1 dimensi
+
+# 3 instansiasi objek
 model = LinearRegression()
 
 st.title("Prediksi Gaji")
 
+# 4 training
 model.fit(X, y)
 input_user = st.number_input("Masukan value :")
 
 prediction = model.predict([[input_user]])
 
+# 6 visualisasi
 fig, ax = plt.subplots()
 
 prediksi_y = model.predict(X)
